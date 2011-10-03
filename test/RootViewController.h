dd6f0c6d885dc387050f016b66d1c8508195b477
akikos <STOKIDIS@IMACI.local>
2011-10-03 19:46:58 +0300

    Initial Commit

---
+++ test/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  test
+//
+//  Created by ΣΑΒΒΑΣ on 3/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
---
+++ test/testAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  testAppDelegate.h
+//  test
+//
+//  Created by ΣΑΒΒΑΣ on 3/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
